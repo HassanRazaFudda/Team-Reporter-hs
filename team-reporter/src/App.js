03d7169a9140c9d2e424d159cab656589b07486f
SalmanKhalil0786 <79781630+SalmanKhalil0786@users.noreply.github.com>
2021-11-26 21:39:31 +0500

    Implement Routing

--- team-reporter/src/App.js
+++ team-reporter/src/App.js
@@ -1,9 +1,15 @@
-import 'antd/dist/antd.css';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+       <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<signUp />} />
+        <Route path="about" element={<logIn />} />
+      </Routes>
     </div>
   );
 }
